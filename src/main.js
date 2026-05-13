8ad60619c059cdb36ed5714cf6122af30e3dd729
weronikakowalskax <w.kowalska190@op.pl>
2026-05-13 20:20:01 +0200

    poprawki

--- src/main.js
+++ src/main.js
@@ -1,62 +1,27 @@
 import dayjs from 'dayjs'
-
 import './style.css'
-import javascriptLogo from './assets/javascript.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import { setupCounter } from './counter.js'
 
-document.querySelector('#app').innerHTML = `
-<section id="center">
-  <div class="hero">
-    <img src="${heroImg}" class="base" width="170" height="179">
-    <img src="${javascriptLogo}" class="framework" alt="JavaScript logo"/>
-    <img src="${viteLogo}" class="vite" alt="Vite logo" />
-  </div>
-  <div>
-    <h1>Get started</h1>
-    <p>Edit <code>src/main.js</code> and save to test <code>HMR</code></p>
-  </div>
-  <button id="counter" type="button" class="counter"></button>
-</section>
+const form = document.getElementById('birthdayForm');
+const bdayInput = document.getElementById('birthdayInput');
+const dialog = document.getElementById('resultDialog');
+const closeButton = document.getElementById('closeDialog');
+const dialogContent = document.getElementById('dialogContent')
 
-<div class="ticks"></div>
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const birthDate = dayjs(bdayInput.value);
+    const today = dayjs();
+    const daysLived = today.diff(birthDate, 'days');
+    const birthdayFunction = birthDate.date() === today.date() && birthDate.month() === today.month();
 
-<section id="next-steps">
-  <div id="docs">
-    <svg class="icon" role="presentation" aria-hidden="true"><use href="/icons.svg#documentation-icon"></use></svg>
-    <h2>Documentation</h2>
-    <p>Your questions, answered</p>
-    <ul>
-      <li>
-        <a href="https://vite.dev/" target="_blank">
-          <img class="logo" src="${viteLogo}" alt="" />
-          Explore Vite
-        </a>
-      </li>
-      <li>
-        <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-          <img class="button-icon" src="${javascriptLogo}" alt="">
-          Learn more
-        </a>
-      </li>
-    </ul>
-  </div>
-  <div id="social">
-    <svg class="icon" role="presentation" aria-hidden="true"><use href="/icons.svg#social-icon"></use></svg>
-    <h2>Connect with us</h2>
-    <p>Join the Vite community</p>
-    <ul>
-      <li><a href="https://github.com/vitejs/vite" target="_blank"><svg class="button-icon" role="presentation" aria-hidden="true"><use href="/icons.svg#github-icon"></use></svg>GitHub</a></li>
-      <li><a href="https://chat.vite.dev/" target="_blank"><svg class="button-icon" role="presentation" aria-hidden="true"><use href="/icons.svg#discord-icon"></use></svg>Discord</a></li>
-      <li><a href="https://x.com/vite_js" target="_blank"><svg class="button-icon" role="presentation" aria-hidden="true"><use href="/icons.svg#x-icon"></use></svg>X.com</a></li>
-      <li><a href="https://bsky.app/profile/vite.dev" target="_blank"><svg class="button-icon" role="presentation" aria-hidden="true"><use href="/icons.svg#bluesky-icon"></use></svg>Bluesky</a></li>
-    </ul>
-  </div>
-</section>
+    if (birthdayFunction) {
+        alert("Wszystkiego najlepszego!");
+    }
+    dialogContent.innerHTML = `Od wpisanej daty do dnia dzisiejszego minęło: <strong>${daysLived}</strong> dni.`;
 
-<div class="ticks"></div>
-<section id="spacer"></section>
-`
+    dialog.showModal();
+});
 
-setupCounter(document.querySelector('#counter'))
+closeButton.addEventListener('click', () => {
+    dialog.close();
+});
